592dd115f1bb4dd21eae582513e692b3f9fca30e
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-17 09:39:02 -0300

    Create distintos.hs

---
+++ TP3/distintos.hs
@@ -0,0 +1,13 @@
+-- Verificar se	todos os elementos	de	uma	lista são distintos.
+
+achaDistinto :: Int -> [Int] -> Bool
+achaDistinto b (a:c)
+  | c==[] = if b == a then False else True
+  | b /= a = achaDistinto b c
+
+distintos :: [Int] -> Bool
+distintos (a:c)
+  | c==[] = True
+  | c/=[] = if achaDistinto a c then distintos c else False
+
+main = print $ distintos [1,2,4,2,5]
