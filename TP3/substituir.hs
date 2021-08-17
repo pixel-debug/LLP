6f23aaf2cfe5d772fabf818e85a6abfbb015d3b0
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-17 08:53:50 -0300

    Create substituir.hs

---
+++ TP3/substituir.hs
@@ -0,0 +1,7 @@
+-- Substituir todos	 elementos	de um determinado valor de uma lista de inteiros	por	um	outro	valor
+
+substituir :: Int->Int->[Int]->[Int]
+substituir a b (x:y) 
+    | a == x = [b] ++ (if y/=[] then substituir a b y else [])
+    | x == x = [x] ++ (if y/=[] then substituir a b y else [])
+main = print $ substituir 1 0 [1,2,1,3,1] 
