9ab8fcaae69ce4335a308b8803c4e3e1202714c6
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-17 08:42:10 -0300

    Haskell programs

---
+++ TP3/potencia.hs
@@ -0,0 +1,9 @@
+--Defina uma função recursiva para o cálculo de	potência de dois números inteiros,
+ onde o primeiro número é elevado ao segundo. Não se pode usar o operador de potência (^).
+
+potencia :: Int->Int->Int
+potencia a b
+  | b > 0 = a*(potencia a (b-1))
+  | b == 0 = 1
+
+main = print $ potencia 2 3
