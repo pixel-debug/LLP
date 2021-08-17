c2d9e04d676defd0a54c43c80e7063b43644c2e3
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-17 08:43:26 -0300

    Update potencia.hs

--- TP3/potencia.hs
+++ TP3/potencia.hs
@@ -1,5 +1,5 @@
 --Defina uma função recursiva para o cálculo de	potência de dois números inteiros,
- onde o primeiro número é elevado ao segundo. Não se pode usar o operador de potência (^).
+-- onde o primeiro número é elevado ao segundo. Não se pode usar o operador de potência (^).
 
 potencia :: Int->Int->Int
 potencia a b
