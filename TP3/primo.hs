ae184302df5eb2019bdec7ad4516e91d54dd88d8
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 14:13:31 -0300

    Update primo.hs

--- TP3/primo.hs
+++ TP3/primo.hs
@@ -1,14 +1,13 @@
 -- Verificar se	um	número é primo
 
-primo :: Int -> Bool
-primo a = achaPrimo a [2, 3 ..]
-
 achaPrimo :: Int -> [Int] ->Bool
 achaPrimo b (c:d)
-    | b <=1 = False
+    | b <= 1 = False                -- numeros iguais e menores que um não são primos
     | b == c = True                 -- igual 
     | (mod b c ) == 0 = False       -- modulo == 0
     | b /= c = achaPrimo b d        -- diferente, manda o 3 ..
     
+primo :: Int -> Bool
+primo a = achaPrimo a [2, 3 ..]
 
 main = print $ primo 17
