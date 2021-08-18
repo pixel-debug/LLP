b5bdfa7d979c04653c0f108f04280bcaa689cae5
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 14:38:06 -0300

    Update intercalar.hs

--- TP3/intercalar.hs
+++ TP3/intercalar.hs
@@ -1,13 +1,11 @@
 -- Dadas duas listas ordenadas de forma	crescente, obter a lista ordenada resultante da intercalação delas.
 
 intercalarListas ::[Int] -> [Int] -> [Int]
-intercalarListas [] []= []
-intercalarListas a [] = a
-intercalarListas [] b = b
-
+intercalarListas [] a = a
+intercalarListas x [] = x
 intercalarListas  (a:b) (x:y)
-   | x <= a = [x] ++ intercalarListas (a:b) y
-   | x >  a = [a] ++ intercalarListas (x:y) b
+   | x <= a = [x] ++ intercalarListas (a:b) y 
+   | otherwise = [a] ++ intercalarListas (x:y) b 
 
 intercalar :: [Int] -> [Int] -> [Int]
 intercalar a b = intercalarListas a b
