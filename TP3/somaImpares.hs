c8642ce4ffa09244c178bf82c38e22b8e27c680c
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 14:14:16 -0300

    Update somaImpares.hs

--- TP3/somaImpares.hs
+++ TP3/somaImpares.hs
@@ -1,8 +1,6 @@
 -- Calcular	o somatório dos	elementos ímpares de uma lista de inteiros
 
 somaImpares [] = 0
-somaImpares (a:x) = if (mod a 2)/=0 
-            then a + somaImpares x else 
-                            0 + somaImpares x
+somaImpares (a:x) = if (mod a 2)/=0 then a + somaImpares x else 0 + somaImpares x
 
 main = print $ somaImpares [1,3,2,7,4,6,5]
