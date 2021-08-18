857121feb6fe42176f9f97510473f62416dc1bb5
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 15:03:29 -0300

    Update somaParciais.hs

--- TP3/somaParciais.hs
+++ TP3/somaParciais.hs
@@ -1,20 +1,16 @@
 -- Calcular	todas as somas	parciais de	uma	lista de inteiros.
   
-
-inverteSoma :: [Int] -> [Int]
-inverteSoma (a:x) = if x/=[] then inverteSoma x ++ [a] 
-                       else [a]
-
-soma :: [Int] -> Int
-soma (a:x) = if x /= [] then a + soma x else a
+soma :: Int -> Int
+soma 1 = 1
+soma x = soma (x - 1) + x
 
 pegaSoma :: [Int] -> [Int]
-pegaSoma (a:x)
-  | x /= [] = [(a + soma x)] ++ pegaSoma x
-  | x == [] = [a]
+pegaSoma [] = []
+pegaSoma (a:x) = [soma a] ++ pegaSoma x
+
 
 somaParciais :: [Int] -> [Int]
-somaParciais x = inverteSoma(pegaSoma(inverteSoma x))
+somaParciais x = pegaSoma x
 
 main = print $ somaParciais [1,2,3,4]
 -- [1, 3, 6, 10]
