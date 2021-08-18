6b96237bfd489241c2f937a6089fa7a2d96fb56a
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 14:32:34 -0300

    Update potencia.hs

--- TP3/potencia.hs
+++ TP3/potencia.hs
@@ -2,8 +2,7 @@
 -- onde o primeiro número é elevado ao segundo. Não se pode usar o operador de potência (^).
 
 potencia :: Int->Int->Int
-potencia a b
-  | b > 0 = a*(potencia a (b-1))
-  | b == 0 = 1
+potencia b 0 = 1
+potencia b e = b * potencia b (e-1)
 
 main = print $ potencia 2 3
