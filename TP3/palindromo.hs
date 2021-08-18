c84f2239ca44035666dadb38c4b75e8fc21e9feb
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 14:04:49 -0300

    Update palindromo.hs

--- TP3/palindromo.hs
+++ TP3/palindromo.hs
@@ -1,15 +1,16 @@
 -- Verificar se	uma	lista de inteiros é palíndromo.
 
+inverte :: [Int] -> [Int]
+inverte (a:x) = if x/=[] then inverte x ++ [a] -- troco os numeros
+                       else [a]
+
 achaPalindromo :: [Int] -> [Int] -> Bool
 achaPalindromo (a:b) (c:d)
   | d == [] = if c == a then True else False                -- ultima iteração
   | d /= [] = if c == a then achaPalindromo b d else False  -- primeiras iterações, se for igual continua procurando até acabar
 
 palindromo :: [Int] -> Bool
-palindromo a = achaPalindromo a (corte a)
-
-corte :: [Int] -> [Int]                                     -- corta a lista na metade
-corte (a:d) = if d/=[] then corte d ++ [a] else [a]
+palindromo a = achaPalindromo a (inverte a)
 
 
 main = print $ palindromo [1,2,3,4,3,2,1]
