355502f81d18a5aad1f6ac64e1eb55dd1b532f18
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-08-18 19:13:28 -0300

    Update palindromo.hs

--- TP3/palindromo.hs
+++ TP3/palindromo.hs
@@ -4,13 +4,8 @@ inverte :: [Int] -> [Int]
 inverte (a:x) = if x/=[] then inverte x ++ [a] -- troco os numeros
                        else [a]
 
-achaPalindromo :: [Int] -> [Int] -> Bool
-achaPalindromo (a:b) (c:d)
-  | d == [] = if c == a then True else False                -- ultima iteração
-  | d /= [] = if c == a then achaPalindromo b d else False  -- primeiras iterações, se for igual continua procurando até acabar
-
 palindromo :: [Int] -> Bool
-palindromo a = achaPalindromo a (inverte a)
+palindromo a = a == inverte a
 
 
 main = print $ palindromo [1,2,3,4,3,2,1]
